fada72bb3c2feb2c22a056868d90115b5f9f9224
Matt <matt.atkins216@gmail.com>
2025-07-31 14:45:02 -0600

    getting finer details in ready for launch

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,55 @@
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
 import './globals.css'
-import { Toaster } from 'react-hot-toast'
-import Navbar from '@/components/ui/Navbar'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700', '800', '900'] })
 
 export const metadata: Metadata = {
-  title: 'Uptime Monitor',
+  title: 'Uptyne',
   description: 'Monitor your websites and get alerts when they go down',
+  icons: {
+    icon: '/favicon.ico',
+  },
+  // openGraph: {
+  //   title: 'Uptyne',
+  //   description: 'Monitor your websites and get alerts when they go down',
+  //   url: 'XXXXXXXXXXXXXXXXXXXXXXXXX',
+  //   siteName: 'Uptyne',
+  //   images: [
+  //     {
+  //       url: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+  //       width: 1200,
+  //       height: 630,
+  //     },
+  //   ],
+  //   locale: 'en-US',
+  //   type: 'website',
+  // },
   
+  // metadataBase: new URL('XXXXXXXXXXXXXXXXXXXXXXXXX'),
+  // alternates: {
+  //   canonical: '/',
+  // },
+  // robots: {
+  //   index: true,
+  //   follow: true,
+  //   nocache: false,
+  //   googleBot: {
+  //     index: true,
+  //     follow: true,
+  //     noimageindex: false,
+  //     'max-video-preview': -1,
+  //     'max-image-preview': 'large',
+  //     'max-snippet': -1,
+  //   },
+  // },
+  // verification: {
+  //   google: 'google-site-verification=google-site-verification',
+  // },
 }
+  
+
 
 export default function RootLayout({
   children,
@@ -24,6 +63,7 @@ export default function RootLayout({
         {children}
         {/* <Toaster position="bottom-right" /> */}
       </body>
+      <GoogleAnalytics gaId="G-L5KLGQWQBC" />
     </html>
   )
 }
--- components/HomeAbout.tsx
+++ components/HomeAbout.tsx
@@ -1,9 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
+import monitors2 from '../public/monitors2.png'
+import monitorID2 from '../public/monitorID2.png'
+
 
 const HomeAbout = () => {
   return (
-      <div className='flex flex-col items-center justify-center px-4 bg-[#2b7fff]/30 h-2/3'>
-      <h2>Uptyne was made for those seeking a value-add uptime monitoring tool for freelancers</h2>
+      <div className='flex flex-col items-center justify-center px-4 bg-[#2b7fff]/20 h-2/3 py-20 gap-6 md:gap-10'>
+      <h2 className='flex text-center font-bold text-4xl md:w-[800px]'>Uptyne was made for those seeking a value-add uptime monitoring tool for freelancers</h2>
+      <Image src={monitorID2} alt='individual monitor screen' width={1200} height={300} className='rounded-lg' />
+
+      <Image src={monitors2} alt="monitoring" width={1200} height={300} className='rounded-lg'/>
     </div>
   )
 }
